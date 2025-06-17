f943b83183382695c72a22515a3f7e41839d2842
dilovskyi <77966432+dilovskyi@users.noreply.github.com>
2025-06-17 12:38:45 +0200

    ssh mit Cimdata Laptop

---
+++ unterrichtsnotizen/woche1/tag1/Main.java
@@ -0,0 +1,9 @@
+package org.unterrichtsnotizen.woche1.tag1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Persone person = new Persone("Petra", "Müller", 33, "Berlin", false);
+        System.out.println(person.alter);
+    }
+}
