c487f6fb8a67a09a4e39d7d716106b0cdca763a1
dilovskyi <77966432+dilovskyi@users.noreply.github.com>
2025-06-17 16:03:39 +0200

    switch-case

---
+++ woche1/schlangenInsel/Aussagen.java
@@ -0,0 +1,15 @@
+package org.woche1.schlangenInsel;
+
+public class Aussagen {
+	public void willkommenText(String str) {
+		System.out.println(str);
+	}
+
+	public void gameGeschichte(String str) {
+		System.out.println(str);
+	}
+
+	public void registrierung(String str) {
+		System.out.println();
+	};
+}
