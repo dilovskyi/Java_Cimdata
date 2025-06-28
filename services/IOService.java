7e48605afacfe01cd740131fab82da46a17fcad8
dilovskyi <77966432+dilovskyi@users.noreply.github.com>
2025-06-29 00:30:41 +0200

    Jackson json parser

---
+++ services/IOService.java
@@ -0,0 +1,19 @@
+package org;
+
+public class IOService {
+    public void intro() {
+        System.out.println(getText("intro"));
+    }
+
+    public void wieHeisstDu() {
+        System.out.println(getText("wieHeisstDu"));
+    }
+
+    public void mottEingeben() {
+        System.out.println(getText("mottEingeben"));
+    }
+
+    private String getText(String key) {
+        return "";
+    }
+}
