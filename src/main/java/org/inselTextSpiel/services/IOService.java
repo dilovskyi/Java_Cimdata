fa11c2a6e0653653d42ede20c530498141fad94e
dilovskyi <77966432+dilovskyi@users.noreply.github.com>
2025-07-02 19:57:11 +0200

    Abarbeitung von JSON-Dateien

--- src/main/java/org/inselTextSpiel/services/IOService.java
+++ src/main/java/org/inselTextSpiel/services/IOService.java
@@ -6,22 +6,13 @@ public class IOService {
     final private Scanner scanner = new Scanner(System.in);
     private JsonParser parser;
 
-    public IOService(String pfad) {
-        this.parser = new JsonParser(pfad);
+    public IOService(JsonParser parser) {
+        this.parser = parser;
     }
 
-    public void ausgabe(String hauptKey) {
-        System.out.println(this.getText(hauptKey));
+    public void ausgabe(String path, String... keys) {
+        System.out.println(parser.getValue(path, keys));
     }
-
-    public void ausgabe(String hauptKey, String innereKey) {
-        System.out.println(this.getText(hauptKey, innereKey));
-    }
-
-    public void ausgabe(String[] keyArray) {
-        System.out.println(this.getText(keyArray));
-    }
-
 
     public String antwortEinlesen() {
         return scanner.nextLine().trim().toLowerCase();
@@ -30,17 +21,4 @@ public class IOService {
     public void trennlinie() {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
-
-    private String getText(String hauptKey) {
-        return parser.getText(hauptKey);
-    }
-
-    private String getText(String hauptKey, String innereKey) {
-        return parser.getText(hauptKey, innereKey);
-    }
-
-    private String getText(String[] keyArray) {
-        return parser.getText(keyArray);
-    }
-
 }
--- src/main/java/org/inselTextSpiel/services/JsonParser.java
+++ src/main/java/org/inselTextSpiel/services/JsonParser.java
@@ -2,79 +2,55 @@ package org.inselTextSpiel.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class JsonParser {
-    private Map introTexte;
-    private Map<String, Object> myMap;
-    private JsonNode rootJsonNode;
+    private final Map<String, JsonNode> jsonDaten = new HashMap<>();
 
-    public JsonParser(String pfad) {
-        jsonLaden(pfad);
-    }
-
-    public String getText(String hauptKey) {
-        return rootJsonNode.path(hauptKey).asText();
-    }
-
-    public String getText(String hauptKey, String innereKey) {
-        return rootJsonNode.path(hauptKey).path(innereKey).asText();
-    }
-
-    public String getText(String[] keyArray) {
-        JsonNode node = rootJsonNode;
-        for (String key : keyArray) {
-            node = node.path(key);
-        }
-        return node.asText();
-    }
-
-
-    // TODO
-    private void jsonNodeParcing(JsonNode node) {
-
-        Iterator<Map.Entry<String, JsonNode>> it = rootJsonNode.fields();
-        while (it.hasNext()) {
-            Map.Entry<String, JsonNode> entry = it.next();
-            Map<String, String> toOut;
-            if (entry.getValue().getNodeType().equals(JsonNodeType.OBJECT)) {
-                System.out.print("Object!");
-            } else {
-                introTexte.put(entry.getKey(), entry.getValue().textValue());
-            }
-            System.out.println(entry.getValue());
-        }
-    }
-
-    private void jsonLaden(String pfad) {
-        ObjectMapper mapper = new ObjectMapper();
+    private JsonNode jsonLaden(String pfad) {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(pfad);
         try {
             if (inputStream != null) {
-                rootJsonNode = mapper.readTree(inputStream);
-//                introTexte = mapper.readValue(inputStream, Map.class);
-//
-//                Iterator<Map.Entry<String, JsonNode>> it = rootJsonNode.fields();
-//                while (it.hasNext()) {
-//                    Map.Entry<String, JsonNode> entry = it.next();
-//                    Map<String, String> toOut;
-//                    if (entry.getValue().getNodeType().equals(JsonNodeType.OBJECT)) {
-//                        System.out.print("Object!");
-//                    } else {
-//                        Map<String, String> ster = new HashMap<String, String>();
-//
-//                    }
-//                }
-
+                return new ObjectMapper().readTree(inputStream);
             } else {
                 throw new RuntimeException(pfad + " nicht gefunden!");
             }
         } catch (Exception e) {
             throw new RuntimeException("Fehler beim Laden der JSON-Datei: " + e.getMessage(), e);
         }
+    }
+
+    private JsonNode lazyLoad(String pfad) {
+        String p = pfad + ".json";
+        JsonNode rootNode;
+        if (!jsonDaten.containsKey(p)) {
+            rootNode = jsonLaden(p);
+            jsonDaten.put(p, rootNode);
+        } else {
+            rootNode = jsonDaten.get(p);
+        }
+        return rootNode;
+    }
+
+    public Map<String, String> getKeyValue(String pfad, String nodeName) {
+        Iterator<Map.Entry<String, JsonNode>> fieldEntry = lazyLoad(pfad).path(nodeName).fields();
+        Map<String, String> result = new HashMap<>();
+        while (fieldEntry.hasNext()) {
+            Map.Entry<String, JsonNode> entry = fieldEntry.next();
+            result.put(entry.getKey(), entry.getValue().asText());
+        }
+        return result;
+    }
+
+    public String getValue(String pfad, String[] keys) {
+        JsonNode rootNode = lazyLoad(pfad);
+        for (String key : keys) {
+            rootNode = rootNode.path(key);
+        }
+        return rootNode.asText();
     }
 }
