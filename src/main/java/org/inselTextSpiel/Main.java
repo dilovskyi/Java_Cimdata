779bba460131c0d5ec62f584b5aa9718d7621fa6
dilovskyi <77966432+dilovskyi@users.noreply.github.com>
2025-07-02 19:59:37 +0200

    Erschaffung von Lokations

--- src/main/java/org/inselTextSpiel/Main.java
+++ src/main/java/org/inselTextSpiel/Main.java
@@ -2,44 +2,46 @@ package org.inselTextSpiel;
 
 import org.inselTextSpiel.charaktere.UserCharakter;
 import org.inselTextSpiel.services.IOService;
+import org.inselTextSpiel.services.JsonParser;
 import org.inselTextSpiel.spiel.Spielwelt;
 
-// Entweden mehrere kleine Dateien oder eine große mit key als "themen".
 public class Main {
     public static void main(String[] args) {
-        Spielwelt spielwelt = new Spielwelt();
-        IOService ioService = new IOService("spielwelt.json");
+        JsonParser parser = new JsonParser();
+        Spielwelt spielwelt = new Spielwelt(parser);
+        IOService ioService = new IOService(parser);
         UserCharakter userCharakter = null;
 
-        ioService.ausgabe("intro");
+        ioService.ausgabe("spielwelt", "intro");
         ioService.trennlinie();
         // -------
-        ioService.ausgabe(new String[]{"registrierung", "frage"});
+        ioService.ausgabe("registrierung", "frage");
         if (ioService.antwortEinlesen().contains("j")) {
-            ioService.ausgabe(new String[]{"registrierung", "antwortJa"});
-            ioService.ausgabe(new String[]{"registrierung", "name"});
+            ioService.ausgabe("registrierung", "antwortJa");
+            ioService.trennlinie();
+            ioService.ausgabe("registrierung", "name");
             userCharakter = new UserCharakter(ioService.antwortEinlesen());
             ioService.trennlinie();
-            ioService.ausgabe("personalBegruesung"); // TODO Keine Namensausgabe
         } else {
-            ioService.ausgabe(new String[]{"registrierung", "antwortNein"});
+            ioService.ausgabe("registrierung", "antwortNein");
         }
         //--------
 
-        ioService.ausgabe(new String[]{"charakter", "eigenschaften"});
+        ioService.ausgabe("charakter", "eigenschaften");
         if (ioService.antwortEinlesen().contains("j") && userCharakter == null) {
-            ioService.ausgabe("keinenChar");
-            ioService.ausgabe(new String[]{"registrierung", "name"});
+            ioService.ausgabe("charakter", "keinenChar");
+            ioService.ausgabe("registrierung", "name");
             userCharakter = new UserCharakter(ioService.antwortEinlesen());
         }
         assert userCharakter != null;
         userCharakter.schowEigenschaften();
         ioService.trennlinie();
-        ioService.ausgabe(new String[]{"charakter", "setMotto"});
+        ioService.ausgabe("charakter", "setMotto");
         userCharakter.setMotto(ioService.antwortEinlesen());
-        ioService.ausgabe(new String[]{"charakter, mottoInfo"});
+        ioService.ausgabe("charakter", "mottoInfo");
         ioService.trennlinie();
-        ioService.ausgabe(new String[]{"locations", "intro"});
+        ioService.ausgabe("locations", "intro");
         System.out.print(userCharakter.getMotto());
+
     }
 }
--- src/main/java/org/inselTextSpiel/spiel/Spielwelt.java
+++ src/main/java/org/inselTextSpiel/spiel/Spielwelt.java
@@ -5,27 +5,30 @@ import org.inselTextSpiel.builders.LocationBuilder;
 import org.inselTextSpiel.locations.Location;
 import org.inselTextSpiel.locations.LocationTyp;
 import org.inselTextSpiel.services.IOService;
+import org.inselTextSpiel.services.JsonParser;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Spielwelt {
-    private ArrayList<Location> besuchteStandorte;
+    private final ArrayList<Location> locationsArrayList = new ArrayList<Location>();
     private LocationTyp aktuellerStandort;
+    private JsonParser parser;
 
 
-    public Spielwelt() {
+    public Spielwelt(JsonParser parser) {
         this.aktuellerStandort = LocationTyp.STRAND;
-        this.besuchteStandorte = new ArrayList<Location>();
+        spielWeltErschaffung(parser);
     }
-
-    public void builderTest() {
-        // Es soll in Spielwelt sei
+    
+    private void spielWeltErschaffung(JsonParser parser) {
         Director director = new Director();
-        LocationBuilder locationBuilder = new LocationBuilder();
-        director.startLocationConstruiren(locationBuilder);
-        Location loc1 = locationBuilder.build();
-        besuchteStandorte.add(loc1);
-        System.out.println(besuchteStandorte.size());
+        LocationBuilder builder = new LocationBuilder();
+        for (LocationTyp locationTyp : LocationTyp.values()) {
+            Map<String, String> locationMap = parser.getKeyValue("locations", locationTyp.name().toLowerCase());
+            director.locationVonMapConstruieren(builder, locationMap);
+            locationsArrayList.add(builder.build());
+        }
     }
 
     public void respawn() {
@@ -33,21 +36,6 @@ public class Spielwelt {
         System.out.println("Du bist am Anfang. Viel Glück!");
     }
 
-    public void setBesuchterStandort() {
-//        besuchteStandorte.add();
-    }
-
-    public void zeigenBesuchteStandorte() {
-        for (Location location : besuchteStandorte) {
-            System.out.println(location);
-        }
-    }
-
-    public void getAlleStandorte() {
-        for (LocationTyp standort : LocationTyp.values()) {
-            System.out.println(standort.ordinal() + " " + standort.titel);
-        }
-    }
 
     private LocationTyp getAktuellerStandort() {
         return this.aktuellerStandort;
